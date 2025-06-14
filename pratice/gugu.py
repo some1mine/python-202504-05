06826646845bff22c855a73a21d95ee8b31f77f9
PARK JAEU <71738723+some1mine@users.noreply.github.com>
2025-06-14 17:04:44 +0900

    Update gugu.py
    One line but not pythonic code

--- pratice/gugu.py
+++ pratice/gugu.py
@@ -1,3 +1,7 @@
-for i in range(3):
-    for j in range(1, 10): print('\t\t'.join([f'{(i * 3 + k)} * {j} = {(i * 3 + k) * j}' for k in range(1, 4)]))
-    print()
+print('\n\n'.join(
+    ['\n'.join([
+        '\t\t'.join([
+            f'{(i * 3 + k)} * {j} = {(i * 3 + k) * j}' 
+        for k in range(1, 4)])
+    for j in range(1, 10)])
+for i in range(3)]))
