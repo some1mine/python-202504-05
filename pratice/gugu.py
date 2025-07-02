5777452aefbdc3b705bc076fccc61f1aa1b8e878
jaeu park <parkjw573@naver.com>
2025-07-02 15:01:17 +0900

    guguk

--- pratice/gugu.py
+++ pratice/gugu.py
@@ -1,7 +1,7 @@
 print('\n\n'.join(
     ['\n'.join([
-        '\t\t\t\t'.join([
-            f'{(i * 3 + k)} * {j} = {(i * 3 + k) * j}' 
+        '\t\t'.join([
+            f'{i * 3 + k} * {j} = {(i * 3 + k) * j}' 
         for k in range(1, 4)])
     for j in range(1, 10)])
 for i in range(3)]))
